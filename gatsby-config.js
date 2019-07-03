1fb17337a361294f56c8d14e8bfe56148a7b5f87
Milan Vodicka <milan.vodicka@gmail.com>
2019-07-03 10:13:16 +0200

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,21 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-142285450-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "remaster.sk",
+      },
+    },
+    {
       resolve: `gatsby-plugin-material-ui`,
       options: {},
     },

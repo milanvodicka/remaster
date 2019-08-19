1a6b1ee3f1c22233083141056794846855056834
Milan Vodicka <milan.vodicka@nike.sk>
2019-08-19 23:06:16 +0200

    Build some of the universal layout components; missing responsivity

--- src/pages/smart-home.js
+++ src/pages/smart-home.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { cloneElement } from "react"
 import Layout from "../components/Layout"
 import Hero from "../components/Hero"
 import image from "../images/smart-home.jpg"
@@ -20,7 +20,18 @@ import assistant from "../images/smart-home/google-assistant.png"
 import alexa from "../images/smart-home/amazon-alexa.png"
 import Transition from "../components/Transition"
 
-const Page = () => {
+const TopSpace = ({ children, margin = false }) => {
+  const theme = useTheme()
+  const space = theme.spacing(9)
+  return cloneElement(children, {
+    style: {
+      ...(margin ? { marginTop: space } : { paddingTop: space }),
+      ...children.props.style,
+    },
+  })
+}
+
+const SideBySide = ({ reverse = false, children, image, alt, style = {} }) => {
   const classes = makeStyles(theme => ({
     image: {
       display: "block",
@@ -28,7 +39,56 @@ const Page = () => {
       height: "auto",
       margin: "auto",
       borderRadius: "5px",
+    }
+  }))();
+
+  const Text = () =>
+    <Grid item sm={6}>
+      <Typography variant="body1">{children}</Typography>
+    </Grid>
+
+  const Image = () =>
+    <Grid item sm={6}>
+      <Transition>
+        <img
+          className={classes.image}
+          src={image}
+          alt={alt}
+        />
+      </Transition>
+    </Grid>
+
+    return (!reverse
+      ? <Grid container alignItems="center" style={style}><Text/><Image/></Grid>
+      : <Grid container alignItems="center" style={style}><Image/><Text/></Grid>
+    );
+}
+
+const WideImage = ({ image, alt, style = {} }) => {
+  const classes = makeStyles({
+    wide: {
+      maxWidth: '100%',
+      padding: 0,
     },
+    image: {
+      width: '100%',
+    }
+  })();
+  return (
+    <Container className={classes.wide} style={style}>
+      <Transition>
+        <img
+          className={classes.image}
+          src={image}
+          alt={alt}
+        />
+      </Transition>
+    </Container>
+  )
+}
+
+const Page = () => {
+  const classes = makeStyles(theme => ({
     workswith: {
       display: "block",
       width: "auto",
@@ -53,97 +113,48 @@ const Page = () => {
         subtitle="Inteligentná domácnosť"
         imageSrc={image}
       />
-      <Container style={{ paddingTop: theme.spacing(9) }}>
-        <Grid container alignItems="center">
-          <Grid item sm={6}>
-            <Typography variant="body1">
-              V dnešnej dobe sme svedkami vzrušujúcich zmien v oblasti
-              domácností a s nimi spojenými technológiami. Všetci sme si zvykli
-              používať smartphone pri každodenných činnostiach a vyhladávaní
-              informácii z celého sveta. Rovnaký trend pozorujeme aj pri
-              ovládaní súčastí bežnej domácnosti, akou sú kúrenie, svetlá,
-              žalúzie, rolety, závlaha, ale aj zabezpečovací, či kamerový
-              systém.
-            </Typography>
-          </Grid>
-          <Grid item sm={6}>
-            <Transition>
-              <img
-                className={classes.image}
-                src={wirelessImage}
-                alt="Bezdrôtová inteligentná domácnosť."
-              />
-            </Transition>
-          </Grid>
-        </Grid>
-
-        <Grid
-          container
-          alignItems="center"
-          style={{ paddingTop: theme.spacing(9) }}
-        >
-          <Grid item sm={6}>
-            <Transition>
-              <img
-                className={classes.image}
-                src={tabletAppImage}
-                alt="Ovládanie inteligentnej domácnosti z tabletu."
-              />
-            </Transition>
-          </Grid>
-          <Grid item sm={6}>
-            <Typography variant="body1">
-              V minulosti bolo veľmi nákladné spojiť tieto oblasti do jedného
-              združeného ovládania. Išlo o rozsiahle systémy, ktoré si
-              vyžadovali dôslednú káblovú predprípravu a náročný návrh. Ku
-              takýmto systémom zákazníci potrebovali spravidla servisnú
-              starostlivosť a každá zmena si vyžadovala zásah programátora.
-            </Typography>
-          </Grid>
-        </Grid>
+      <Container>
+        <TopSpace>
+          <SideBySide image={wirelessImage} alt="Bezdrôtová inteligentná domácnosť.">
+            V dnešnej dobe sme svedkami vzrušujúcich zmien v oblasti
+            domácností a s nimi spojenými technológiami. Všetci sme si zvykli
+            používať smartphone pri každodenných činnostiach a vyhladávaní
+            informácii z celého sveta. Rovnaký trend pozorujeme aj pri
+            ovládaní súčastí bežnej domácnosti, akou sú kúrenie, svetlá,
+            žalúzie, rolety, závlaha, ale aj zabezpečovací, či kamerový
+            systém.
+          </SideBySide>
+        </TopSpace>
+        <TopSpace>
+          <SideBySide reverse image={tabletAppImage} alt="Ovládanie inteligentnej domácnosti z tabletu.">
+            V minulosti bolo veľmi nákladné spojiť tieto oblasti do jedného
+            združeného ovládania. Išlo o rozsiahle systémy, ktoré si
+            vyžadovali dôslednú káblovú predprípravu a náročný návrh. Ku
+            takýmto systémom zákazníci potrebovali spravidla servisnú
+            starostlivosť a každá zmena si vyžadovala zásah programátora.
+          </SideBySide>
+        </TopSpace>
       </Container>
 
-      <Container className={classes.wide}>
-        <Typography variant="h5" style={{ textAlign: "center" }}>
-          Veľkou výhodou je absolútna flexibilita a sloboda, v prípade zmien,
-          alebo rozšírenia systému o ďalšie prvky.
-        </Typography>
-      </Container>
+      <TopSpace>
+        <WideImage image={mobileAppImage} alt="Wide"/>
+      </TopSpace>
 
-      <Container
-        style={{
-          marginTop: theme.spacing(9),
-        }}
-      >
-        <Grid
-          container
-          alignItems="center"
-          style={{ paddingTop: theme.spacing(9) }}
-        >
-          <Grid item sm={6}>
-            <Typography variant="body1">
-              Dnes sú možnosti naklonené širokej verejnosti. Či už sa jedná o
-              malý byt s ovládaním radiatorových hlavíc, svietidiel a
-              audio-video zariadení, alebo rodinný dom s kompletnou
-              inteligenciou, ktorý vie presne čo má kedy urobiť, aj bez Vášho
-              zásahu. Navyše nie je potrebná rozsiahla predpríprava ani ťahanie
-              kabeláže a je možné systém nasadiť aj na pôvodnú
-              elektroinštaláciu. Umožńujú nám to moderné bezdrôtové technológie
-              ako je Z-WAVE, Wi-Fi, ZigBee a iné. Navyše ovládanie je čím ďalej
-              tým jednoduchšie, a to pomocou smarthone, tabletu, alebo hlasového
-              ovládania Alexa, Google Assistant a Siri.
-            </Typography>
-          </Grid>
-          <Grid item sm={6}>
-            <Transition onEnterViewport={() => console.log("foo")}>
-              <img
-                className={classes.image}
-                src={mobileAppImage}
-                alt="Mobilná aplikácia na ovládanie inteligentnej domácnosti."
-              />
-            </Transition>
-          </Grid>
-        </Grid>
+      <Container>
+        <TopSpace>
+          <SideBySide image={mobileAppImage} alt="Mobilná aplikácia na ovládanie inteligentnej domácnosti.">
+            Dnes sú možnosti naklonené širokej verejnosti. Či už sa jedná o
+            malý byt s ovládaním radiatorových hlavíc, svietidiel a
+            audio-video zariadení, alebo rodinný dom s kompletnou
+            inteligenciou, ktorý vie presne čo má kedy urobiť, aj bez Vášho
+            zásahu. Navyše nie je potrebná rozsiahla predpríprava ani ťahanie
+            kabeláže a je možné systém nasadiť aj na pôvodnú
+            elektroinštaláciu. Umožńujú nám to moderné bezdrôtové technológie
+            ako je Z-WAVE, Wi-Fi, ZigBee a iné. Navyše ovládanie je čím ďalej
+            tým jednoduchšie, a to pomocou smarthone, tabletu, alebo hlasového
+            ovládania Alexa, Google Assistant a Siri.
+          </SideBySide>
+        </TopSpace>
       </Container>
 
       <Container className={classes.wide}>
